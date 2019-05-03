c17f02c84e6876323d8e02f3998bbf7db7fe5de6
Oleg <omikula123@gmail.com>
2019-05-03 15:31:10 +0300

    03.05.2019

--- GUI.py
+++ GUI.py
@@ -10,11 +10,9 @@ window.geometry('480x320')
 
 tempDef = 20
 
-selection = 0
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code {0}".format(str(rc)))
-    client.subscribe("set/Temp")
+    client.subscribe("get/Temp")
 
 def on_message(client, userdata, msg):
     print("Message received-> " + msg.topic + " " + str(msg.payload))
@@ -25,19 +23,21 @@ async def getdata():
     client.username_pw_set(username="username", password="password")
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect('192.168.0.103', 1883)
+    client.connect('192.168.1.103', 1883)
     client.loop_start()
 
 def senddata(data):
     clientSend = mqtt.Client("Python")
     clientSend.username_pw_set(username="username", password="password")
-    clientSend.connect('192.168.0.103', 1883)
+    clientSend.connect('192.168.1.103', 1883)
     clientSend.publish(topic="set/Temp", payload=data)
     clientSend.disconnect()
 
 def sel():
     global selection
-    selection = var.get()
+    scaleData = str(var.get())
+    radioData = var2.get()
+    selection = scaleData + "   " + str(radioData)
     senddata(selection)
 
 
@@ -48,11 +48,19 @@ switchMode1.place(x = 165, y = 220)
 lblTemp = Label(window, font="Arial 25", text = tempDef)
 lblTemp.place(x = 210, y = 130)
 
+
+var2 = IntVar()
+R1 = Radiobutton(window, text="Default", variable=var2, value=1)
+R1.place(x = 140, y = 50)
+R2 = Radiobutton(window, text="Night", variable=var2, value=2)
+R2.place(x = 240, y = 50)
+
 var = DoubleVar()
 scale = Scale( window, variable = var,length = 300, orient = HORIZONTAL, resolution=0.5, from_ = 15, to = 40)
 scale.place(x = 90, y = 180)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(getdata())
+R1.select()
 loop.close()
 window.mainloop()
--- Pub.py
+++ Pub.py
@@ -1,9 +1,9 @@
 import paho.mqtt.client as mqtt
 
-broker_address = "192.168.0.103"
+broker_address = "192.168.1.103"
 broker_portno = 1883
 
 client = mqtt.Client("Python")
 client.username_pw_set(username="username", password="password")
 client.connect(broker_address, broker_portno)
-client.publish(topic = "set/Temp", payload = "sdssdsf")
+client.publish(topic = "set/Temp", payload = "Roma")
